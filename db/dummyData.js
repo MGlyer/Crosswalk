787632e6f2966f495ae71a6a1cacbf0a5070d949
Martin <MartinGlyer@gmail.com>
2018-12-26 18:54:32 -0500

    writes out some dummy and Lorem data

--- db/dummyData.js
+++ db/dummyData.js
@@ -1,6 +1,72 @@
+let loremData = require('./loremIpsum')
+
+const genLorem = () => {
+  let ind = Math.floor(Math.random() * loremData.length)
+  return loremData[ind]
+}
+
+let pathways = [
+  {
+    name: 'Food Science, Dietetics, and Nutrition',
+    code: '200',
+    descriptor: genLorem(),
+    Top4Codes: ['1005, 1006, 1009']
+  },
+  {
+    name: 'Food Services and Hospitality',
+    code: '201',
+    descriptor: genLorem(),
+    Top4Codes: ['1002, 1006, 1010']
+  },
+  {
+    name: 'Hospitality, Tourism, and Recreation',
+    code: '202',
+    descriptor: genLorem(),
+    Top4Codes: ['1005, 1011, 1012']
+  },
+]
+
+let topCode2 = [
+  {name: 'Agriculture and Natural Resources', code: null},
+  {name: 'Architecture and Environmental Design', code: null},
+  {name: 'Environmental Sciences and Technologies', code: null},
+  {name: 'Biological Sciences', code: null},
+  {name: 'Business and Management', code: null},
+  {name: 'Media and Communications', code: null},
+  {name: 'Information Technology', code: null},
+  {name: 'Education', code: null},
+  {name: 'Engineering and Industrial Technologies', code: null},
+  {name: 'Fine and Applied Arts', code: null},
+  {name: 'Foreign Language', code: null},
+  {name: 'Health', code: null},
+  {name: 'Family and Consumer Sciences', code: null},
+  {name: 'Law', code: null},
+  {name: 'Humanities (Letters)', code: null},
+  {name: 'Library Science', code: null},
+  {name: 'Mathamatics', code: null},
+  {name: 'Military Studies', code: null},
+  {name: 'Physical Sciences', code: null},
+  {name: 'Psychology', code: null},
+  {name: 'Public and Protective Services', code: null},
+  {name: 'Social Sciences', code: null},
+  {name: 'Commercial Servies', code: null},
+]
+let topCode4 = []
+let K12HighSchools = []
+
+const fillTopCode2 = () => {
+  for (var i = 0; i < 21; i++) {
+    let num = i < 11 ? '0' + (i+1) : i+1
+    topCode2[i].code = num
+  }
+}
+
+
+fillTopCode2()
+
 module.exports = {
-  pathways: [],
-  topCode2: [],
-  topCode4: [],
-  K12HighSchools: []
+  pathways,
+  topCode2,
+  topCode4,
+  K12HighSchools
 }
---
+++ db/loremIpsum.js
@@ -0,0 +1,14 @@
+module.exports = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis lacus lorem, commodo luctus facilisis eu, commodo ac turpis.',
+  'In feugiat, ex vitae elementum elementum, risus arcu iaculis orci, id auctor urna odio nec arcu.',
+  'Praesent commodo, sapien vitae lobortis varius, velit arcu rhoncus nibh, ac euismod erat tortor at lorem. Pellentesque non enim magna.',
+  'Sed pharetra turpis urna, nec malesuada massa imperdiet ac. Praesent nec sapien a lacus maximus facilisis. Fusce ullamcorper sit amet justo a vestibulum.',
+  'Maecenas vitae sapien ligula. Integer lectus dolor, rutrum a metus sit amet, finibus ullamcorper nunc.',
+  'Nullam egestas lorem eget leo pellentesque, non laoreet leo vehicula. Donec condimentum suscipit quam ut consequat. Nunc lobortis est dignissim pulvinar dictum.',
+  'Sed quis eros vitae nisl iaculis gravida ut id magna. Nulla a vehicula dolor. Aliquam sagittis luctus ex, sit amet porta nisl scelerisque vitae.',
+  'Sed nec magna nisl. Donec vitae quam tincidunt, feugiat est ac, fermentum orci. Vivamus condimentum sodales mi.',
+  'Proin sit amet turpis condimentum, dictum felis ut, vulputate massa. Donec volutpat ornare est, id laoreet nisl commodo eget.',
+  'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+  'Etiam egestas pharetra laoreet. In ex quam, ullamcorper fermentum volutpat ut, bibendum ut magna. Nulla facilisi.',
+  'Ut commodo molestie enim ut vehicula. Donec quis consectetur felis, eu bibendum metus.'
+]
